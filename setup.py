ec4f33472939025056fee2aeb98df1f9bd253ce9
Ed Solis <Ed@ALGLO.com>
2016-07-23 16:16:14 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+config = {
+    'description':      'My Project',
+    'author':           'Ed Solis',
+    'url':              'URL to get at it.',
+    'download_url':     'Where to download it.',
+    'author_email':     'edsfocci@gmail.com',
+    'version':          '0.0.1',
+    'install_requires': ['nose'],
+    'packages':         ['NAME'],
+    'scripts':          [],
+    'name':             'projectname'
+}
+
+setup(**config)
